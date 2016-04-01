ba57f051dff26293291852dc9a338042fb1c874e
Richard Temple <richardtemple@mutexmusic.com>
2016-04-01 13:49:17 -0700

    About Triangle Project 2 Finished

--- triangle.rb
+++ triangle.rb
@@ -14,13 +14,26 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  if a == b && a == c
-    return :equilateral
-  elsif a == b || a == c || c == b
-    return :isosceles
-  else
-    return :scalene
-  end
+
+  raise(TriangleError) if [a, b, c].any? { |num| num < 1 }
+
+  x, y, z = [a, b, c].sort
+  raise(TriangleError) if x + y <= z
+
+  return :equilateral if a == b && a == c
+
+  return :isosceles if a == b || a == c || c == b
+
+  return :scalene if a != b && a != c && b != c
+
+
+    raise(TriangleError, "That's one funky triangle")
+
+  # much more elegant solution to mine:
+  # raise TriangleError if [a,b,c].min <= 0
+  # x, y, z = [a,b,c].sort
+  # raise TriangleError if x + y <= z
+  # [:equilateral,:isosceles,:scalene].fetch([a,b,c].uniq.size - 1)
 end
 
 # Error class used in part 2.  No need to change this code.
